666d56baebed5a436cd9dc85e729ca8a100118d6
S <30459885+tacticstactics@users.noreply.github.com>
2022-06-29 14:16:05 +0900

    begin

--- PythonApplication1/PythonApplication1/Polarimeter_def.py
+++ PythonApplication1/PythonApplication1/Polarimeter_def.py
@@ -1,7 +1,63 @@
 #Polarimeter_def.py
 
 
+import numpy as np
 
 
+def proc1(param=0.01,m=256):
+
+
+    tcol = np.zeros((m,1)); # time
+    
+    Etcol = np.ones(m, dtype=complex);#*2
+
+       
+    Signalcol = np.ones(m, dtype=complex);#*2
+
+
+    freq = 4
+    omega = 2*np.pi*freq
+ 
+    print('omega=', omega, 'Hz')
+
+    #Pulsewidth
+    tau = 0.4 
+
+    # PD signal. Proportional to optical power
+    #Signal = 0
+
+
+    for ii in range(m):
+
+
+        delay1 = 0.025*ii-1.6      
+
+        s = 0
+        
+        for jj in range(m):
+
+            time1 = 0.025*jj-1.6
+            tcol[(jj)] = time1
+            
+            Et = np.exp(1j * omega * time1) * np.exp(-1.38 * (time1/tau)**2)
+            
+            Etcol[(jj)] = Et
+            
+            #Et = math.sin(omega * time1)     
+            
+
+            td = time1 + delay1
+
+            Etd = np.exp(1j * omega * td) * np.exp(-1.38 * (td/tau)**2)                          
+     
+
+            s += (abs(Et+Etd)**2)**2            
+
+              #Signalcol[(ii)] = Etd
+            Signalcol[(ii)] = s
+
+                    #print(Signalcol)
+
+    return tcol, Etcol,Signalcol
 
 
--- PythonApplication1/PythonApplication1/Polarimeter_main.py
+++ PythonApplication1/PythonApplication1/Polarimeter_main.py
@@ -1,5 +1,34 @@
 #Polarimeter_main.py
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+import Polarimeter_def
+
+param = 0.01
+m = 128
+
+tcol, Etcol, Signalcol = Polarimeter_def.proc1(param,m)
+
+
+print('')
+print('Polarimeter_main.py')
+print('')
+
+
+fig = plt.figure(figsize = (10,6), facecolor='lightblue')
+
+ax1 = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2)
+ax3 = fig.add_subplot(2, 2, 3)
+ax4 = fig.add_subplot(2, 2, 4)
+
+ax1.plot(tcol,np.real(Etcol))
+ax2.plot(tcol,np.imag(Etcol))
+ax3.plot(tcol,np.real(Signalcol))
+
+plt.show()
+
 
 
