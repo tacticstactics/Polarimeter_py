df4d59f4d251ac50c664ef36e7c75116051d9c23
S <30459885+tacticstactics@users.noreply.github.com>
2022-06-30 17:02:50 +0900

    q

--- PythonApplication1/PythonApplication1/Polarimeter_def.py
+++ PythonApplication1/PythonApplication1/Polarimeter_def.py
@@ -1,74 +1,23 @@
 #Polarimeter_def.py
 
-
 import numpy as np
+import math
 
+def propagate(wavel = 1.55e-6,no=1,opl=1,Ein):
 
-def propagate(wavel=1.55e-6,no,octwp, Ein):
+    T11 = np.array([[np.exp(1j * wavel * no * opl), 0],[0, np.exp(1j * wavel * no * opl)]]);
 
+    Eout = T11 * Ein
 
-    tcol = np.zeros((m,1)); # time
-    
-    Etcol = np.ones(m, dtype=complex);#*2
-
-       
-    Signalcol = np.ones(m, dtype=complex);#*2
-
-
-    freq = 4
-    omega = 2*np.pi*freq
- 
-    print('omega=', omega, 'Hz')
-
-    #Pulsewidth
-    tau = 0.4 
-
-    # PD signal. Proportional to optical power
-    #Signal = 0
-
-
-    for ii in range(m):
-
-
-        delay1 = 0.025*ii-1.6      
-
-        s = 0
-        
-        for jj in range(m):
-
-            time1 = 0.025*jj-1.6
-            tcol[(jj)] = time1
-            
-            Et = np.exp(1j * omega * time1) * np.exp(-1.38 * (time1/tau)**2)
-            
-            Etcol[(jj)] = Et
-            
-            #Et = math.sin(omega * time1)     
-            
-
-            td = time1 + delay1
-
-            Etd = np.exp(1j * omega * td) * np.exp(-1.38 * (td/tau)**2)                          
-     
-
-            s += (abs(Et+Etd)**2)**2            
-
-              #Signalcol[(ii)] = Etd
-            Signalcol[(ii)] = s
-
-                    #print(Signalcol)
-
-    return tcol, Etcol,Signalcol
+    return Eout
 
 
 
 def faradayrotaor(theta1, Ein):
 
 
-     T11 = [cos(theta1*pi/180), sin(theta1*pi/180);
-   	-sin(theta1*pi/180), cos(theta1*pi/180)];
+     T11 = np.array([[math.cos(theta1*math.pi/180), math.sin(theta1*math.pi/180)],[-1*math.sin(theta1*math.pi/180), math.cos(theta1*math.pi/180)]]);
 
-     Ea = T11 * Ein ;
-
- return Ea
+     Eout = T11 * Ein
+     return Eout
 
--- PythonApplication1/PythonApplication1/Polarimeter_main.py
+++ PythonApplication1/PythonApplication1/Polarimeter_main.py
@@ -7,32 +7,24 @@ import matplotlib.pyplot as plt
 import Polarimeter_def
 
 wavel = 1.55e-6
-no = 1
+
+def new_func():
+    no = 1
+    return no
+
+no = new_func()
+
 opl = 1
 
-Ein = [[0.0], [0.0]]  # initialization of presets is done in __init__
+Ein = [[1], [0]]  # initialization of presets is done in __init__
+
+print(Ein)
 
 
-tcol, Etcol, Signalcol = Polarimeter_def.propagate(wavel=1.55e-6,no,opl, Ein)
+Eout = Polarimeter_def.propagate(wavel,no,opl,Ein)
 
 
 print('')
-print('Polarimeter_main.py')
+print(Eout)
 print('')
-
-
-fig = plt.figure(figsize = (10,6), facecolor='lightblue')
-
-ax1 = fig.add_subplot(2, 2, 1)
-ax2 = fig.add_subplot(2, 2, 2)
-ax3 = fig.add_subplot(2, 2, 3)
-ax4 = fig.add_subplot(2, 2, 4)
-
-ax1.plot(tcol,np.real(Etcol))
-ax2.plot(tcol,np.imag(Etcol))
-ax3.plot(tcol,np.real(Signalcol))
-
-plt.show()
-
-
 
