e64b7eae478a1ff536a4165182503a6271aac29d
S <30459885+tacticstactics@users.noreply.github.com>
2022-08-24 10:35:13 +0900

    1

--- PythonApplication1/PythonApplication1/Polarimeter_def.py
+++ PythonApplication1/PythonApplication1/Polarimeter_def.py
@@ -28,7 +28,7 @@ def faradayrotaor(theta1=45, Ein=np.array([[1],[0]])):
 
  
 
- def waveplate(wavel=1.55e-6,no=1,ne=1.1,opl=1,theta1=45,Ein=np.array([[1],[0]])):
+def waveplate(wavel=1.55e-6,no=1,ne=1.1,opl=1,theta1=45,Ein=np.array([[1],[0]])):
 
    WP1 = np.array([[math.cos(theta1*math.pi/180),math.sin(theta1*math.pi/180)],[-1*math.sin(theta1*math.pi/180),math.cos(theta1*math.pi/180)]]);
 
