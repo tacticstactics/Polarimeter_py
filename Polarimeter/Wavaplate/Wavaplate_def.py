d2b154e19db849e14a7ff0816ba9038224421bf3
S <30459885+tacticstactics@users.noreply.github.com>
2022-11-29 17:11:22 +0900

    1

---
+++ Polarimeter/Wavaplate/Wavaplate_def.py
@@ -0,0 +1,13 @@
+
+#Waveplate_main.py
+
+
+print('Waveplate_main.py')
+
+import numpy as np
+
+from scipy.fftpack import fft, fftshift
+from scipy import signal
+
+import matplotlib.pyplot as plt
+import Polarimeter_def
